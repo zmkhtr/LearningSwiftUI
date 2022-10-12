23243df226831a5b1484715d2894bcb57da28009
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2022-10-12 19:38:29 +0700

    Initial new project

---
+++ LearningSwiftUI/LearningSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LearningSwiftUI
+//
+//  Created by PT.Koanba on 12/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
