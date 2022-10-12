951423299c0aa747a4d62efd9c362a20045d6507
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2022-10-12 20:13:25 +0700

    Create a Custom Circle ImageView

---
+++ LearningSwiftUI/LearningSwiftUI/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  LearningSwiftUI
+//
+//  Created by PT.Koanba on 12/10/22.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
