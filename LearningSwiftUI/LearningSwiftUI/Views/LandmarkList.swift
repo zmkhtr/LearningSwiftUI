43983ccb8d574d36855d09c44eaa7461750e8131
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2022-10-12 20:35:41 +0700

    Create static list of landmarks

---
+++ LearningSwiftUI/LearningSwiftUI/Views/LandmarkList.swift
@@ -0,0 +1,23 @@
+//
+//  LandmarkList.swift
+//  LearningSwiftUI
+//
+//  Created by PT.Koanba on 12/10/22.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarks[0])
+            LandmarkRow(landmark: landmarks[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
