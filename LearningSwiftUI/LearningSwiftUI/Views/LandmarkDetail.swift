5b6064a2c7ebad8402cbeb59bb0b4df17e326dbc
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2022-10-12 20:48:10 +0700

    Pass data into child view, detail view showing the detail of selected item from list

--- LearningSwiftUI/LearningSwiftUI/Views/LandmarkDetail.swift
+++ LearningSwiftUI/LearningSwiftUI/Views/LandmarkDetail.swift
@@ -8,40 +8,44 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
-        VStack {
-            MapView()
-                .ignoresSafeArea(edges: .top)
-                .frame(height: 300)
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("Calirfornia")
+        ScrollView {
+            VStack {
+                MapView(coordinate: landmark.locationCoordinate)
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+                CircleImage(image: landmark.image)
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                VStack(alignment: .leading) {
+                    Text(landmark.name)
+                        .font(.title)
+                    HStack {
+                        Text(landmark.park)
+                        Spacer()
+                        Text(landmark.state)
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    
+                    Divider()
+                    
+                    Text("About \(landmark.name)")
+                        .font(.title2)
+                    Text(landmark.description)
                 }
-                .font(.subheadline)
-                .foregroundColor(.secondary)
-                
-                Divider()
-                
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
+                .padding()
             }
-            .padding()
-            
-            Spacer()
+            .navigationTitle(landmark.name)
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
 
 struct Detail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarks[0])
     }
 }
