bd23eb0f0d4cf3b7e68295ffbaead2a1dd375319
zmkhtr <33575723+zmkhtr@users.noreply.github.com>
2022-10-12 22:02:37 +0700

    Create a Category View

---
+++ LearningSwiftUI/LearningSwiftUI/Views/Categories/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  LearningSwiftUI
+//
+//  Created by PT.Koanba on 12/10/22.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
